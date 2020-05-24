7667dbd39a9a815869d9e9d40ccce0b39d1838f9
Ono <soichrooono@gmail.com>
2020-05-24 07:09:36 +0000

    [Add]Post show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '56ccf168be47452caee73d5f4972f1ebffae9130e4bc9884dd7bc73cb608226dc84a38de36a3d83ec20896f2bcee599e91b9e804604247519e9e50f49be7474e'
