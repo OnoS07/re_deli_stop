c4e9ff04c3fddf6b1c520ed1828a1cf787c2966c
Ono <soichrooono@gmail.com>
2020-05-25 03:05:34 +0000

    [Add]Post validates,flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,12 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(post_params)
 		@post.user_id = current_user.id
-		@post.save
-		redirect_to post_path(@post)
+		if @post.save
+			flash[:notice] = "NEW POST CREATE !"
+			redirect_to post_path(@post)
+		else
+			render("posts/new")
+		end
 	end
 
 	def edit
@@ -25,8 +29,12 @@ class PostsController < ApplicationController
 
 	def update
 		@post = Post.find(params[:id])
-		@post.update(post_params)
-		redirect_to post_path(@post)
+		if @post.update(post_params)
+			flash[:notice] = "UPDATE YOUR POST !"
+			redirect_to post_path(@post)
+		else
+			render("posts/edit")
+		end
 	end
 
 	def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
 	attachment :post_image
 	belongs_to :user
+
+	validates :title, presence: true ,length: {maximum: 20}
+	validates :body, presence: true ,length: {maximum: 100}
+	validates :post_image, presence: true
 end
