b94cc6635a3dd4f7dd92fc4b452f3148cecbaf20
Ono <soichrooono@gmail.com>
2020-05-26 03:58:42 +0000

    [Add]いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
 	attachment :post_image
 	belongs_to :user
     has_many :post_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+    def favorited_by?(user)
+    	favorites.where(user_id: user.id).exists?
+    end
 
 	validates :title, presence: true ,length: {maximum: 20}
 	validates :body, presence: true ,length: {maximum: 100}
